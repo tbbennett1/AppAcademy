1488a3aa5aaddd9ade8fb8dbd046b1ddbbc3a40b
bbennett <tbbennett1@gmail.com>
2019-10-29 12:59:18 -0700

    queue project

--- queue_project/lib/queue.js
+++ queue_project/lib/queue.js
@@ -36,8 +36,32 @@ class Queue {
 
   enqueue(val){
     const newNode = new Node(val);
+    if(!this.front){
+      this.front = newNode;
+      this.back= newNode;
+    }else{
+      this.back.next = newNode;
+      this.back = newNode;
+    }
+    return ++this.length;
+  }
 
-    
+  dequeue(){
+    if(!this.front){
+      return null;
+    }
+
+    const temp = this.front;
+    if(this.front === this.back){
+      this.back = null;
+    }
+    this.front = this.front.next;
+    this.length--;
+    return temp.value;
+  }
+
+  size(){
+    return this.length;
   }
 }
 
