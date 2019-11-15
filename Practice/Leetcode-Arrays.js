c0ffb83d36f7d07dbb416e7654c438a6ba6cb058
bbennett <tbbennett1@gmail.com>
2019-11-15 14:16:59 -0800

    array-rotate

--- Practice/Leetcode-Arrays.js
+++ Practice/Leetcode-Arrays.js
@@ -25,3 +25,32 @@ var removeDuplicates = function (nums) {
 };
 
 // console.log(removeDuplicates(nums)) //=> 5
+
+// Rotate Array
+// Given an array, rotate the array to the right by k steps, where k is non - negative.
+//   Example 1:
+// Input: [1, 2, 3, 4, 5, 6, 7] and k = 3
+// Output: [5, 6, 7, 1, 2, 3, 4]
+// Explanation:
+// rotate 1 steps to the right: [7, 1, 2, 3, 4, 5, 6]
+// rotate 2 steps to the right: [6, 7, 1, 2, 3, 4, 5]
+// rotate 3 steps to the right: [5, 6, 7, 1, 2, 3, 4]
+//  Example 2:
+// Input: [-1, -100, 3, 99] and k = 2
+// Output: [3, 99, -1, -100]
+// Explanation:
+// rotate 1 steps to the right: [99, -1, -100, 3]
+// rotate 2 steps to the right: [3, 99, -1, -100]
+const nums = [1, 2, 3, 4, 5, 6, 7]
+var rotate = function(nums, k){
+  let arr = [];
+  for(let i = 0; i < nums.length; i++){
+    arr[(i + k) % nums.length] = nums[i];
+  }
+  for(let i = 0; i < nums.length; i++){
+    nums[i] = arr[i];
+  }
+}
+
+rotate(nums, 3)
+console.log(nums)
