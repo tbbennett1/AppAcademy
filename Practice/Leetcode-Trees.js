97ce89abfe581b81f2ab9084db5efb8cb7de6fb6
bbennett <tbbennett1@gmail.com>
2019-11-21 15:45:30 -0800

    BST Symmetric

--- Practice/Leetcode-Trees.js
+++ Practice/Leetcode-Trees.js
@@ -34,3 +34,16 @@ var maxDepth = function (root) {
 
   return max;
 };
+
+// Symmetric Tree
+// Given a binary tree, check whether it is a mirror of itself(ie, symmetric around its center).
+// For example, this binary tree[1, 2, 2, 3, 4, 4, 3] is symmetric:
+//     1
+//    / \
+//   2    2
+//  / \   / \
+// 3   4  4   3
+
+var isSymmetric = function (root) {
+  
+};
