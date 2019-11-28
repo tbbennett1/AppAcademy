b045bf9ba22923a6cbe0d88ae3a3787eddd3d4b4
bbennett <tbbennett1@gmail.com>
2019-11-27 17:28:27 -0800

    merge sorted array

--- Practice/Leetcode-Sorting-Searching.js
+++ Practice/Leetcode-Sorting-Searching.js
@@ -6,7 +6,7 @@
 //   Example:
 // Input:
 let nums1 = [1, 2, 3, 0, 0, 0];
-let m = 3;
+let m = 6;
 let nums2 = [2, 5, 6];
 let n = 3;
 // Output: [1, 2, 2, 3, 5, 6]
@@ -15,7 +15,8 @@ var merge = function (nums1, m, nums2, n) {
   let i = 0;
   let j = 0;
   while(i < m && j < n){
-    if(nums1[i] < nums2[j]){
+    if(nums1[i] < nums2[j] && nums1[i] !== 0){
+      console.log(nums1[i])
       i++;
     }else{
       nums1.splice(i, 0, nums2[j])
